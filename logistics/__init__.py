2a9abeabb1b8a60ca8ffaa1582d67ab3b776b440
anyesgray38 <anyesgray38@gmail.com>
2026-09-01 17:13:12 -0400

    Build 06: export commercial intelligence

--- logistics/__init__.py
+++ logistics/__init__.py
@@ -1,46 +1,14 @@
 """Core logistics domain package."""
 
-from .dispatch import (
-    DispatchAudit,
-    DispatchCandidate,
-    DispatchConfig,
-    DispatchFeasibility,
-    DispatchPlan,
-    assign_best_vehicle,
-    audit_candidate,
-    check_feasibility,
-    plan_multi_stop,
-    score_candidate,
-)
+from .commercial import CommercialAudit, CustomerScore, LaneRateAnalysis, LoadQuote, OpportunityScore, analyze_lane, audit_opportunity, quote_load, rank_opportunity, score_customer
+from .dispatch import DispatchAudit, DispatchCandidate, DispatchConfig, DispatchFeasibility, DispatchPlan, assign_best_vehicle, audit_candidate, check_feasibility, plan_multi_stop, score_candidate
 from .models import Customer, Location, Shipment, TimeWindow, Vehicle
 from .tracking import EtaEstimate, TrackingAudit, TrackingEvent, TrackingProvider, TrackingSnapshot, audit_tracking, reduce_tracking
 from .validation import ValidationIssue, ValidationResult, validate_shipment, validate_vehicle
 
 __all__ = [
-    "Customer",
-    "Location",
-    "Shipment",
-    "TimeWindow",
-    "Vehicle",
-    "ValidationIssue",
-    "ValidationResult",
-    "validate_shipment",
-    "validate_vehicle",
-    "DispatchAudit",
-    "DispatchCandidate",
-    "DispatchConfig",
-    "DispatchFeasibility",
-    "DispatchPlan",
-    "assign_best_vehicle",
-    "audit_candidate",
-    "check_feasibility",
-    "plan_multi_stop",
-    "score_candidate",
-    "EtaEstimate",
-    "TrackingAudit",
-    "TrackingEvent",
-    "TrackingProvider",
-    "TrackingSnapshot",
-    "audit_tracking",
-    "reduce_tracking",
+    "Customer", "Location", "Shipment", "TimeWindow", "Vehicle", "ValidationIssue", "ValidationResult", "validate_shipment", "validate_vehicle",
+    "DispatchAudit", "DispatchCandidate", "DispatchConfig", "DispatchFeasibility", "DispatchPlan", "assign_best_vehicle", "audit_candidate", "check_feasibility", "plan_multi_stop", "score_candidate",
+    "EtaEstimate", "TrackingAudit", "TrackingEvent", "TrackingProvider", "TrackingSnapshot", "audit_tracking", "reduce_tracking",
+    "CommercialAudit", "CustomerScore", "LaneRateAnalysis", "LoadQuote", "OpportunityScore", "analyze_lane", "audit_opportunity", "quote_load", "rank_opportunity", "score_customer",
 ]
