5e6c86fdabbaa3c3748fdaccfe6d7e884250310d
anyesgray38 <anyesgray38@gmail.com>
2026-09-01 17:11:04 -0400

    Build 05: export tracking primitives

--- logistics/__init__.py
+++ logistics/__init__.py
@@ -13,6 +13,7 @@ from .dispatch import (
     score_candidate,
 )
 from .models import Customer, Location, Shipment, TimeWindow, Vehicle
+from .tracking import EtaEstimate, TrackingAudit, TrackingEvent, TrackingProvider, TrackingSnapshot, audit_tracking, reduce_tracking
 from .validation import ValidationIssue, ValidationResult, validate_shipment, validate_vehicle
 
 __all__ = [
@@ -35,4 +36,11 @@ __all__ = [
     "check_feasibility",
     "plan_multi_stop",
     "score_candidate",
+    "EtaEstimate",
+    "TrackingAudit",
+    "TrackingEvent",
+    "TrackingProvider",
+    "TrackingSnapshot",
+    "audit_tracking",
+    "reduce_tracking",
 ]
